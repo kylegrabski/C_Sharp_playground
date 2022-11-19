f54a357c5f486adc0464faeaa96c9b3fd9fe6ad1
kylegrabski <kyle.grabski@gmail.com>
2022-11-18 18:40:07 -0700

    add method calls

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            var book = new Book("Kyle's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            
+            var stats = book.GetStatistics();
+
+            System.Console.WriteLine($"the book name IS {book.bookName}");
+
+            System.Console.WriteLine($"The lowest grade is {stats.Low}");
+            System.Console.WriteLine($"The highest grade is {stats.High}");
+            System.Console.WriteLine($"The average grade is {stats.Average}");
         }
     }
 }
