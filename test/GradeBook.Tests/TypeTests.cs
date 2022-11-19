cdb010389c615028987661ec7887a49cd4d2b362
kylegrabski <kyle.grabski@gmail.com>
2022-11-18 18:49:37 -0700

    update tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -13,13 +13,15 @@ public class TypeTests
         var book1 = GetBook("Book 1");
         var book2 = GetBook("Book 2");
         var book3 = GetBook("Book 2");
+        var book4 = book3;
 
         // Act
 
         // Assert
         Assert.Equal("Book 1", book1.Name);
         Assert.Equal("Book 2", book2.Name);
-        Assert.Equal(book3, book2);
+        Assert.Same(book3, book4);
+        Assert.Same(book3, book2);
 
     }
 
