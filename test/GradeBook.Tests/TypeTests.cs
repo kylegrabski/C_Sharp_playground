4591283e0ae29937c4a5fc238605c5b74546242d
kylegrabski <kyle.grabski@gmail.com>
2023-01-03 16:55:58 -0700

    adjust tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -12,17 +12,30 @@ public class TypeTests
         // Arrange
         var book1 = GetBook("Book 1");
         var book2 = GetBook("Book 2");
-        var book3 = GetBook("Book 2");
-        var book4 = book3;
 
         // Act
 
         // Assert
         Assert.Equal("Book 1", book1.Name);
         Assert.Equal("Book 2", book2.Name);
-        Assert.Same(book3, book4);
-        Assert.Same(book3, book2);
+        Assert.NotSame(book1, book2);
 
+    }
+
+    [Fact]
+    public void TwoVarsCanReferenceSameObject()
+    {
+        // Arrange
+        var book1 = GetBook("Book 1");
+        var book2 = book1;
+
+        Assert.Same(book1, book2);
+
+        // Act
+
+        // Assert
+        Assert.Same(book1, book2);
+        Assert.True(Object.ReferenceEquals(book1, book2));
     }
 
     Book GetBook(string name)
