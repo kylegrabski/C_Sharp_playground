748cb5b1ae3aa7d9f01d039fccebb8b2a010d63d
kylegrabski <kyle.grabski@gmail.com>
2023-02-06 17:01:56 -0700

    add new test

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -8,6 +8,58 @@ public class TypeTests
 {
     [Fact]
     public void Test1()
+    {
+        var x = GetInt();
+
+        SetInt(ref x);
+
+        Assert.Equal(42, x);
+    }
+
+    private void SetInt(ref int x)
+    {
+        x = 42;
+    }
+
+    private int GetInt()
+    {
+        return 3;
+    }
+
+    [Fact]
+    public void CSharpCanPassByRef()
+     {
+        // Arrange
+        var book1 = GetBook("Book 1");
+        GetBookSetName(ref book1, "New Name");
+
+        // Assert
+        Assert.Equal("New Name", book1.Name);
+    }
+
+    private void GetBookSetName(ref Book book, string name)
+    {
+        book = new Book(name);
+    }
+
+    [Fact]
+    public void CSharpIsPassByValue()
+     {
+        // Arrange
+        var book1 = GetBook("Book 1");
+        GetBookSetName(book1, "New Name");
+
+        // Assert
+        Assert.Equal("Book 1", book1.Name);
+    }
+
+    private void GetBookSetName(Book book, string name)
+    {
+        book = new Book(name);
+    }
+
+    [Fact]
+    public void CanSetNameFromReference()
      {
         // Arrange
         var book1 = GetBook("Book 1");
